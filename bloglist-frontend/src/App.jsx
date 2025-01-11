a30fb91e4b04467049a85c0c575b6cd4a3957d20
santerilukka <156800715+santerilukka@users.noreply.github.com>
2025-01-11 19:37:51 +0200

    5.3: blogilistan frontend, step3

--- bloglist-frontend/src/App.jsx
+++ bloglist-frontend/src/App.jsx
@@ -6,7 +6,7 @@ import loginService from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
-  const [newBlog, setNewBlog] = useState('')
+  const [newBlog, setNewBlog] = useState({title: '', author: '', url: ''})
   const [errorMessage, setErrorMessage] = useState(null)
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
@@ -56,6 +56,29 @@ const App = () => {
     blogService.setToken(null)
   }
 
+  const addBlog = async (event) => {
+    event.preventDefault()
+    try {
+      const createdBlog = await blogService.create(newBlog)
+      setBlogs(blogs.concat(createdBlog))
+      setNewBlog({title: '', author: user.name, url: ''})
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+
+    } catch (exception) {
+      setErrorMessage('Error adding blog')
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+    }
+  }
+
+  const handleBlogChange = (event) => {
+    const {name, value} = event.target
+    setNewBlog({...newBlog, [name]: value})
+  }
+
   const loginForm = () => (
     <div>
       <h2>Log in to application</h2>
@@ -84,11 +107,37 @@ const App = () => {
     </div>
   )
 
+  const blogForm = () => (
+    <form onSubmit={addBlog}>
+      <h2>Create new blog</h2>
+      <div>
+        title:
+        <input
+          type="text"
+          name="title"
+          value={newBlog.title}
+          onChange={handleBlogChange}
+        />
+      </div>
+      <div>
+        url:
+        <input
+          type="text"
+          name="url"
+          value={newBlog.url}
+          onChange={handleBlogChange}
+        />
+      </div>
+      <button type="submit">create</button>
+    </form>
+  )
+
   const blogList = () => (
     <div>
-      <h2>blogs</h2>
+      <h2>Blogs</h2>
       <p>{user.name} logged in</p>
       <button onClick={handleLogout}>logout</button>
+      {blogForm()}
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
--- bloglist-frontend/src/components/Blog.jsx
+++ bloglist-frontend/src/components/Blog.jsx
@@ -1,6 +1,6 @@
 const Blog = ({ blog }) => (
   <div>
-    {blog.title} {blog.author}
+    {blog.title} {blog.author} - added by {blog.user.name}
   </div>  
 )
 
