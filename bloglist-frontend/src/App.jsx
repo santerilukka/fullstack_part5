d516e2a7cb41b3517928807b07e48f645748a475
santerilukka <156800715+santerilukka@users.noreply.github.com>
2025-01-15 18:54:36 +0200

    5.10: blogilistan frontend, step10

--- bloglist-frontend/src/App.jsx
+++ bloglist-frontend/src/App.jsx
@@ -151,8 +151,10 @@ const App = () => {
       <p>{user.name} logged in</p>
       <button onClick={handleLogout}>logout</button>
       {blogForm()}
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} handleLike={handleLike} />
+      {blogs
+        .sort((a, b) => b.likes - a.likes)
+        .map(blog =>
+          <Blog key={blog.id} blog={blog} handleLike={handleLike} />
       )}
     </div>
   )
