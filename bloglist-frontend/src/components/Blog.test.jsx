91d87524bcc58ffb1360dc5be34bb94130cd2a6a
santerilukka <156800715+santerilukka@users.noreply.github.com>
2025-01-31 16:26:14 +0200

    5.15: blogilistan testit, step3

--- bloglist-frontend/src/components/Blog.test.jsx
+++ bloglist-frontend/src/components/Blog.test.jsx
@@ -51,3 +51,32 @@ test('url, number of likes and user are displayed when the button is pressed', a
   expect(screen.getByText('likes 0')).toBeInTheDocument()
   expect(screen.getByText('Test User')).toBeInTheDocument()
 })
+
+test('like button is pressed twice', async () => {
+  const blog = {
+    title: 'Test Blog',
+    author: 'Test Author',
+    url: 'http://testurl.com',
+    likes: 0,
+    user: {
+      name: 'Test User',
+      username: 'testuser'
+    }
+  }
+
+  const loggedInUser = {
+    username: 'testuser'
+  }
+
+  const mockHandler = vi.fn()
+
+  render(<Blog blog={blog} loggedInUser={loggedInUser} handleLike={mockHandler}/>)
+  const user = userEvent.setup()
+  const button = screen.getByText('view')
+  await user.click(button)
+  const likeButton = screen.getByText('like')
+  await user.click(likeButton)
+  await user.click(likeButton)
+
+  expect(mockHandler.mock.calls).toHaveLength(2)
+})
