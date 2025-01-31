8ce1048fc96dea4e5c731238e5f479bf0f11d58c
santerilukka <156800715+santerilukka@users.noreply.github.com>
2025-01-31 16:21:59 +0200

    5.14: blogilistan testit, step2

--- bloglist-frontend/src/components/Blog.test.jsx
+++ bloglist-frontend/src/components/Blog.test.jsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
 test('renders content', () => {
@@ -22,4 +23,31 @@ test('renders content', () => {
 
   const titleElement = container.querySelector('div')
   expect(titleElement).toHaveTextContent('Test Blog')
+})
+
+test('url, number of likes and user are displayed when the button is pressed', async () => {
+  const blog = {
+    title: 'Test Blog',
+    author: 'Test Author',
+    url: 'http://testurl.com',
+    likes: 0,
+    user: {
+      name: 'Test User',
+      username: 'testuser'
+    }
+  }
+
+  const loggedInUser = {
+    username: 'testuser'
+  }
+
+  render(<Blog blog={blog} loggedInUser={loggedInUser} />)
+
+  const user = userEvent.setup()
+  const button = screen.getByText('view')
+  await user.click(button)
+
+  expect(screen.getByText('http://testurl.com')).toBeInTheDocument()
+  expect(screen.getByText('likes 0')).toBeInTheDocument()
+  expect(screen.getByText('Test User')).toBeInTheDocument()
 })
